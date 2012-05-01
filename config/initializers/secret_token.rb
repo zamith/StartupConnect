14db3972228635fa9fab9aa3d47d1ff693ae415e
Zamith <zamith.28@gmail.com>
2012-05-01 17:50:24 +0100

    Starting Startup Connect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartupConnect::Application.config.secret_token = '32ffc0c3bbe71bcd608846997f21b0a496a60fe46fef181b91d37af6386f9f6cdf9f181d28d34bc1aed51a3a4945f9b15c6bb2912cc95ac8a1e3109bdbbd555b'
